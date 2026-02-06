ec52547e0ce8739c3b389de3c023687ee437b9a6
ahnafzaki12 <ahnafzaki.b@gmail.com>
2026-02-06 11:47:29 +0700

    theme2

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
-import { AuthProvider } from './hooks/AuthContext'
+import { AuthProvider } from './context/AuthContext'
 import './index.css'
+import { ThemeProvider } from './context/ThemeContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </AuthProvider>
   </StrictMode>
 )
